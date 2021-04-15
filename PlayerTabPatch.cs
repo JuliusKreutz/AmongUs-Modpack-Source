9e236b56897bc210f84e38512e5cca6a8bb1c078
Julius Kreutz <jk@julius-kreutz.de>
2021-04-15 12:07:00 +0200

    no overlap

---
+++ PlayerTabPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using SaveManager = BLCGIFOPMIA;
+
+namespace Modpack
+{
+    public class PlayerTabPatch
+    {
+
+        [HarmonyPatch(typeof(PlayerTab), nameof(PlayerTab.OnEnable))]
+        public static class OnEnablePatch
+        {
+            public static void Postfix(PlayerTab __instance)
+            {
+                for (int i = 0; i < __instance.ColorChips.Count; i++)
+                {
+                    var chip = __instance.ColorChips.ToArray()[i];
+                    chip.transform.localScale *= 0.65f;
+                }
+            }
+        }
+
+    }
+}
